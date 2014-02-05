20884cca7174754fa53373fa7e2301dcbebc54a7
Oted <herr.olle@gmail.com>
2014-02-05 17:50:47 +0100

    add some lines for parsin a midi file

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,17 +2,18 @@ package main;
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.Play;
+import jm.util.Read;
+import jm.util.View;
 
 public class Main implements JMC{
-	
-	public static void main(String[] args) {
+            
+    public static void main(String[] args) {
 		System.out.println("Wow. Such music program. Much code.");
-		Play.midi(new Note(C4, QN));
-		Play.midi(new Note(C4, QN));
-		Play.midi(new Note(D4, QN));
-		Play.midi(new Note(D4, QN));
-		Play.midi(new Note(F4, QN));
-
-	}
+		Score score = new Score("m83");
+		Read.midi(score, "/home/oted/workspace/evoMusic/midifiles/m83-midnight_city.mid");
+		Part score2 = score.getPart(1);
+		System.out.println(score2.getTitle());
+		Play.midi(score2);
+    }
 
 }
