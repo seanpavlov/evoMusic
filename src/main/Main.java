aad54ab787a6bd3afed3cc9926e22b0de0ece512
Christian Svensson <crillesvensson22@gmail.com>
2014-02-05 16:44:51 +0100

    jMusic basic test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
+import jm.JMC;
+import jm.music.data.*;
+import jm.util.Play;
 
-public class Main {
+public class Main implements JMC{
 	
 	public static void main(String[] args) {
 		System.out.println("Wow. Such music program. Much code.");
+		Play.midi(new Note(C4, QN));
+		Play.midi(new Note(C4, QN));
+		Play.midi(new Note(D4, QN));
+		Play.midi(new Note(D4, QN));
+		Play.midi(new Note(F4, QN));
+
 	}
 
 }
