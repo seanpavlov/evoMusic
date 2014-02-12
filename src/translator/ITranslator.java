89b8663ec7a76c72bcbff5373885856267c5dfba
Viktor Anderling <natanande@gmail.com>
2014-02-12 22:10:45 +0100

    Make Translator use ITranslator

--- src/translator/ITranslator.java
+++ src/translator/ITranslator.java
@@ -1,7 +1,24 @@
 package translator;
 
+import structure.Song;
+
 public interface ITranslator {
 	
+    /**
+     * Loads a MIDI file and call the constructor of song object
+     * on load success
+     * 
+     * @param path, relative path to MIDI file
+     */
 	public Song loadMidi(String path);
 	
+	
+    /**
+     * Function takes a path to save the MIDI file to, and the object which 
+     * to save and saves it.
+     * 
+     * @param path, where to save
+     * @param song, song object to unload
+     */
+    public void saveMidi(String path, Song song);
 }
