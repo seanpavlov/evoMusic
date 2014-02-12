9961927235dcd642a6ebe482cd0f9126e71c06a9
Viktor Anderling <natanande@gmail.com>
2014-02-12 22:05:58 +0100

    move Song to package structure

--- src/structure/Song.java
+++ src/structure/Song.java
@@ -1,9 +1,17 @@
+package structure;
 import jm.music.data.Score;
 
-
+/**
+ * A class that inherits most of its properties from the Score class.
+ *
+ */
 public class Song extends Score {
 	
-	
+	/**
+	 * Don't know about this one >_>
+	 */
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * Creates a new Song object by copying all content from the given score.
 	 * 
