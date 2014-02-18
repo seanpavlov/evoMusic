b5f3435525ef5cfff1b6a1d3554f6baf30fcc986
Christian Svensson <crillesvensson22@gmail.com>
2014-02-18 14:20:08 +0100

    Add implementation to create song object from db record

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,12 +1,17 @@
 package database;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.List;
 
 import model.Song;
+import model.translator.Translator;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.WriteResult;
@@ -54,24 +59,25 @@ public class Database implements IDatabase{
     }
 
     @Override
-    public BasicDBObject createDBObject(Song song) {
+    public DBObject createDBObject(Song song) {
         //TODO implement method
         return null;
     }
 
     @Override
-    public Song createSongObject(BasicDBObject dbDoc) {
-        //TODO implement method
-        return null;
+    public Song createSongObject(BasicDBObject dbDoc) throws IOException {
+        String songPath = (String)dbDoc.get(MIDI_PATH_KEY);
+        Song song = Translator.INSTANCE.loadMidiToSong(songPath);
+        return song;
     }
 
     @Override
-    public void saveDbObject(BasicDBObject dbDoc) { 
+    public void saveDbObject(DBObject dbDoc) { 
         //TODO implement method
     }
 
     @Override
-    public BasicDBObject retreiveDBObject() {
+    public DBObject retreiveDBObject() {
         //TODO implement method
         return null;
     }
@@ -93,6 +99,22 @@ public class Database implements IDatabase{
                     .append(MIDI_PATH_KEY, song.getPath())
                     .append(USER_TAGS_KEY, song.getUserTags());
         return songs.insert(songData);
+    }
+
+    @Override
+    public List<Song> retrieveSongs() throws IOException {
+        List<Song> listOfSongs = new LinkedList<Song>();
+        DBCursor cursor = songs.find();
+        
+        try {
+            while(cursor.hasNext()) {
+                listOfSongs.add(this.createSongObject((BasicDBObject) cursor.next()));
+            }
+         } finally {
+            cursor.close();
+         }
+        
+        return listOfSongs;
     }
     
 }
