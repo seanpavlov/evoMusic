986807d17632c403849e9f19a8bafb85bd9b62ef
Viktor Anderling <natanande@gmail.com>
2014-02-19 13:48:40 +0100

    Add rate function to Rater

--- src/rating/Rater.java
+++ src/rating/Rater.java
@@ -1,9 +1,15 @@
 package rating;
 
+import model.Song;
+
 public class Rater {
     public Rater(){
         
         
+    }
+    
+    public double rate(Song song) {
+        return 0;
     }
     
 }
