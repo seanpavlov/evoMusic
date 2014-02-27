b87d6b43e660851f858052867cb8d652b299fdc7
Oted <herr.olle@gmail.com>
2014-02-27 16:10:17 +0100

    now able to rate tracks with translator

--- src/com/evoMusic/model/Translator.java
+++ src/com/evoMusic/model/Translator.java
@@ -1,7 +1,11 @@
 package com.evoMusic.model;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
+import com.evoMusic.model.enumerators.TrackTag;
+
+import jm.music.data.Part;
 import jm.music.data.Score;
 import jm.util.Play;
 import jm.util.Read;
@@ -17,11 +21,43 @@ public enum Translator  {
      * 
      * @param path, relative path to MIDI file
      */
-    public Song loadMidiToSong(String path) throws IOException {
+    public Song loadMidiToSong(String path){
         Score score = new Score();
         Read.midi(score, path);
         
         return new Song(score);
+    }
+    
+    /**
+     * Loads a MIDI file and call the constructor of song object once every track 
+     * is tagged, the none tag is used for all irrelevant tracks
+     * 
+     * @param path to load from
+     * @return new song object
+     */
+    public Song loadMidiToSongAndTagTracks(String path){
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        Score score = new Score();
+        Read.midi(score, path);
+        Song song = new Song(score);
+        System.out.println("Please tag these tracks corresponding tracktag, use none for irrelevant tracks");
+        
+        for (Part p : song.getScore().getPartArray()){
+            Translator.INSTANCE.showPart(p);
+            String tag = sc.next().toString().toUpperCase();
+            
+            while (!TrackTag.contains(tag)){
+                System.out.println("No such TrackTag, enter again");
+                tag = sc.next();
+            }
+            
+            TrackTag t = TrackTag.valueOf(tag);
+            song.addTagToTrack(p, t);
+            i++;
+        }
+        
+        return song;
     }
 
     /**
@@ -40,7 +76,7 @@ public enum Translator  {
         File outputFile = null;
         String path = "";
         do {
-            path = "./output/" + name + (copy != 0 ? "-"+copy : "")+ ".midi";
+            path = "./output/" + name + (copy != 0 ? "-"+copy : "")+ ".mid";
             outputFile = new File(path); 
                 // if dupe, filename is appended "-1"
             copy++;
@@ -72,9 +108,28 @@ public enum Translator  {
     /**
      * Show the structure of the song in JMusics built in MIDI display
      * 
-     * @param song to be played
+     * @param song to be shown
      */
     public void showSong(Song song){
         View.show(song.getScore());
     }
+    
+    /**
+     * Show the structure of the part in JMusics built in MIDI display
+     * 
+     * @param part to be shown
+     */
+    public void showPart(Part part){
+        View.sketch(part);
+    }
+    
+    /**
+     * Closes all showing windows 
+     */
+    public void closeDisplayWindow(){
+       // View.
+    }
+    
+    
+
 }
--- src/com/evoMusic/model/enumerators/TrackTag.java
+++ src/com/evoMusic/model/enumerators/TrackTag.java
@@ -4,4 +4,12 @@ public enum TrackTag {
 	MELODY, CHORDS,
 	RHYTHM, BEAT,
 	DRUMS,   NONE;
+	
+	public static boolean contains(String tag){
+	    for (TrackTag t : TrackTag.values()){
+	        if (t.toString().equals(tag.toUpperCase()))
+	            return true;
+	    }
+	    return false;
+	}
 }
