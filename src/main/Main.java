5c937c2d3a17b130bdda503b4536da3dd73675ce
Sean Pavlov <sean.pavlov@gmail.com>
2014-03-02 21:55:50 +0100

    Add a suited main for Mutation.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,33 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Song;
+import mutation.ISubMutator;
 import mutation.Mutator;
+import mutation.ScaleOfFifthMutator;
+import mutation.SimplifyMutator;
 import translator.Translator;
 import crossover.Crossover;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // String megamanPath = "midifiles/mm2wily1.mid";
-        // Song testSong = Translator.INSTANCE.loadMidiToSong(megamanPath);
-        // Translator.INSTANCE.playSong(testSong);
         Song super_mario_bros_theme = Translator.INSTANCE
                 .loadMidiToSong("midifiles/super_mario_bros_theme.mid");
         Song super_mario_world_overview  = Translator.INSTANCE
                 .loadMidiToSong("midifiles/super_mario_world_overworld.mid");
-//        Crossover crossover = new Crossover(new Song[] { super_mario_bros_theme, super_mario_world_overview});
-//        crossover.setNumberOfIntersections(8);
-//        Song crossedSong = crossover.makeCrossover();
-        Mutator m = new Mutator(0.8, super_mario_bros_theme);
-        Song mutatedSong = m.mutate(super_mario_bros_theme);
-        Translator.INSTANCE.playSong(mutatedSong);
+        Song avicii_levels  = Translator.INSTANCE
+                .loadMidiToSong("midifiles/avicii_levels.mid");
+        List<ISubMutator> subMutators = new ArrayList<>();
+        subMutators.add(new ScaleOfFifthMutator(0.5, 1));
+        subMutators.add(new SimplifyMutator(0.3, 2, 1));
+        Mutator m = new Mutator(subMutators, 0.5);
+        m.mutate(super_mario_bros_theme);
+        Translator.INSTANCE.playSong(super_mario_bros_theme);
 //        Translator.INSTANCE.saveSongToMidi(mutatedSong, "hej");
 //        Translator.INSTANCE.showSong(mutatedSong);
     }
-
 }
