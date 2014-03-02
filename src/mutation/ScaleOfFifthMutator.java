3430a049998e7c7c45e5c01e6e9b6c9ffa0bdd38
Sean Pavlov <sean.pavlov@gmail.com>
2014-03-02 20:16:23 +0100

    Add scale of fifth mutator as a separate class which works with MIDI numbers instead of frequency.

---
+++ src/mutation/ScaleOfFifthMutator.java
@@ -0,0 +1,47 @@
+package mutation;
+
+import jm.music.data.Note;
+import model.Song;
+
+public class ScaleOfFifthMutator extends ISubMutator {
+    private int stepRange;
+
+    public ScaleOfFifthMutator(double mutationProbability, int stepRange) {
+        super(mutationProbability);
+        this.stepRange = stepRange;
+    }
+
+    /**
+     * Mutate the note in the scale of fifth, which is five half steps in either
+     * direction.
+     */
+    @Override
+    public void mutate(Song song, int noteIndex) {
+        if (Math.random() < this.getProbability()) {
+            MidiUtil mu = new MidiUtil();
+            Note note = song.getScore().getPart(0).getPhrase(0).getNote(noteIndex);
+            int nbrOfSteps = (int) ((Math.random() * stepRange) + 1);
+            int pitchNbr = note.getPitch();
+            System.out.print("Old pitch: " + note.getPitch());
+            if (!mu.isCorrupt(pitchNbr)) {
+                if (mu.canRaiseNote(pitchNbr, nbrOfSteps)) {
+                    if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
+                        if (Math.random() < 0.5) {
+                            note.setPitch(pitchNbr - (5 * nbrOfSteps));
+                        } else {
+                            note.setPitch(pitchNbr + (5 * nbrOfSteps));
+                        }
+                    } else {
+                        note.setPitch(pitchNbr + (5 * nbrOfSteps));
+                    }
+                } else if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
+                    note.setPitch(pitchNbr - (5 * nbrOfSteps));
+                }
+                System.out.println("New pitch: " + note.getPitch());
+                song.getScore().getPart(0).getPhrase(0)
+                        .setNote(note, noteIndex);
+            }
+        }
+    }
+
+}
