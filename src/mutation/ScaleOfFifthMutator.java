8cfb53219ef833dfc518b523c5c29025115b8c80
Sean Pavlov <sean.pavlov@gmail.com>
2014-03-02 21:51:16 +0100

    Delete check of is blank since Mutator handle it now.

--- src/mutation/ScaleOfFifthMutator.java
+++ src/mutation/ScaleOfFifthMutator.java
@@ -6,41 +6,48 @@ import model.Song;
 public class ScaleOfFifthMutator extends ISubMutator {
     private int stepRange;
 
+    /**
+     * Mutate the note in the scale of fifth, which is five half steps in either
+     * direction. Takes in stepRange which will change the pitch within the
+     * given range.
+     * 
+     * @param mutationProbability
+     *            is the probability of using this mutator
+     * @param stepRange
+     *            is the range that it can mutate the pitch within the rules of
+     *            scale of fifths.
+     */
     public ScaleOfFifthMutator(double mutationProbability, int stepRange) {
         super(mutationProbability);
         this.stepRange = stepRange;
     }
 
     /**
-     * Mutate the note in the scale of fifth, which is five half steps in either
-     * direction.
+     * Mutate the note with noteIndex of song.
      */
     @Override
     public void mutate(Song song, int noteIndex) {
         if (Math.random() < this.getProbability()) {
             MidiUtil mu = new MidiUtil();
-            Note note = song.getScore().getPart(0).getPhrase(0).getNote(noteIndex);
+            Note note = song.getScore().getPart(0).getPhrase(0)
+                    .getNote(noteIndex);
             int nbrOfSteps = (int) ((Math.random() * stepRange) + 1);
             int pitchNbr = note.getPitch();
-            System.out.print("Old pitch: " + note.getPitch());
-            if (!mu.isBlank(pitchNbr)) {
-                if (mu.canRaiseNote(pitchNbr, nbrOfSteps)) {
-                    if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
-                        if (Math.random() < 0.5) {
-                            note.setPitch(pitchNbr - (5 * nbrOfSteps));
-                        } else {
-                            note.setPitch(pitchNbr + (5 * nbrOfSteps));
-                        }
+            if (mu.canRaiseNote(pitchNbr, nbrOfSteps)) {
+                if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
+                    if (Math.random() < 0.5) {
+                        note.setPitch(pitchNbr - (5 * nbrOfSteps));
                     } else {
                         note.setPitch(pitchNbr + (5 * nbrOfSteps));
                     }
-                } else if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
-                    note.setPitch(pitchNbr - (5 * nbrOfSteps));
+                } else {
+                    note.setPitch(pitchNbr + (5 * nbrOfSteps));
                 }
-                System.out.println("New pitch: " + note.getPitch());
-                song.getScore().getPart(0).getPhrase(0)
-                        .setNote(note, noteIndex);
+            } else if (mu.canLowerNote(pitchNbr, nbrOfSteps)) {
+                note.setPitch(pitchNbr - (5 * nbrOfSteps));
             }
+            song.getScore().getPart(0).getPhrase(0).setNote(note, noteIndex);
+
         }
     }
 
