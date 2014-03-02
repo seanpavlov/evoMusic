c8c0c69066a3b4fc8b716e6fae30b147d32ef8db
Sean Pavlov <sean.pavlov@gmail.com>
2014-03-02 23:34:25 +0100

    Add all classes again because someone moved the packages.

---
+++ src/com/evoMusic/model/geneticAlgorithm/mutation/SimplifyMutator.java
@@ -0,0 +1,57 @@
+package com.evoMusic.model.geneticAlgorithm.mutation;
+
+import jm.music.data.Note;
+import com.evoMusic.model.*;
+
+public class SimplifyMutator extends ISubMutator {
+
+    private int nbrOfPastNeighbours;
+    private double neighbourProbability;
+
+    /**
+     * Mutate so that past neighboring notes will have the same pitch as the
+     * current node.
+     * 
+     * @param mutationProbability
+     *            is the probability of this mutator to run.
+     * @param nbrOfPastNeighbours
+     *            is the number of past neighbors who can be mutated.
+     * @param neighbourProbability
+     *            is the probability of the specific neighbor to the mutated.
+     */
+    public SimplifyMutator(double mutationProbability, int nbrOfPastNeighbours,
+            double neighbourProbability) {
+        super(mutationProbability);
+        this.nbrOfPastNeighbours = nbrOfPastNeighbours;
+        this.neighbourProbability = neighbourProbability;
+    }
+
+    /**
+     * Mutate the note with noteIndex of song.
+     */
+    @Override
+    public void mutate(Song song, int noteIndex) {
+        Note note = song.getScore().getPart(0).getPhrase(0).getNote(noteIndex);
+        if (Math.random() < this.getProbability()) {
+            MidiUtil mu = new MidiUtil();
+            int currentNoteIndex = noteIndex - 1;
+            if(currentNoteIndex >= 0){
+                noteIteration: for (int i = 0; i < nbrOfPastNeighbours; i++) {
+                    while (mu.isBlank(song.getScore().getPart(0).getPhrase(0).getNote(currentNoteIndex).getPitch()) && currentNoteIndex >= 0) {
+                        currentNoteIndex--;
+                    }
+                    if (currentNoteIndex < 0) {
+                        break noteIteration;
+                    } else {
+                        if (Math.random() < neighbourProbability) {
+                            song.getScore().getPart(0).getPhrase(0)
+                                    .getNote(currentNoteIndex)
+                                    .setPitch(note.getPitch());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
