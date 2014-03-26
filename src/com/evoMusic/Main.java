d801b7f7ea6cce5218aff24a7c8cf9f1e0d3cf93
Christian Svensson <crillesvensson22@gmail.com>
2014-03-26 14:01:32 +0100

    create class to represent chord

--- src/com/evoMusic/Main.java
+++ src/com/evoMusic/Main.java
@@ -1,6 +1,11 @@
 package com.evoMusic;
 
+import jm.music.data.Part;
+import jm.util.View;
+
 import com.evoMusic.controller.InputController;
+import com.evoMusic.model.Song;
+import com.evoMusic.model.geneticAlgorithm.rating.ChordRepetitionRater;
 import com.evoMusic.util.Translator;
 
 public class Main {
@@ -10,7 +15,9 @@ public class Main {
     public static void main(String[] args) {
         //new InputController(args);
         
-        Song song = Translator.INSTANCE.loadMidiToSong(path);
-        
+        Song song = Translator.INSTANCE.loadMidiToSong("midifiles/Elton_John-Candle_in_the_Wind.mid");
+        ChordRepetitionRater rater = new ChordRepetitionRater();
+        Part part = song.getScore().getPart(0);
+        rater.ratePart(part);
     }
 }
--- src/com/evoMusic/model/geneticAlgorithm/rating/ChordRepetitionRater.java
+++ src/com/evoMusic/model/geneticAlgorithm/rating/ChordRepetitionRater.java
@@ -1,6 +1,7 @@
 package com.evoMusic.model.geneticAlgorithm.rating;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,35 +29,118 @@ public class ChordRepetitionRater extends SubRater{
     
     public double ratePart(Part part){
         Phrase[] phrases = part.getPhraseArray();
+        Map<Double, List<Integer>> notePitches = new HashMap<Double, List<Integer>>();
         Map<Double, List<Note>> chords = new HashMap<Double, List<Note>>();
-        
+        System.out.println(phrases.length);
         for(Phrase phrase : phrases){
             Note[] notes = phrase.getNoteArray();
-            for(Note note : notes){
-                double start = note.getSampleStartTime();
+            double pStart = phrase.getStartTime();
+            for(int i = 0; i < notes.length; i++){
+                Note note = notes[i];
+                if(note.getNote().equals("N/A"))
+                    continue;
+                double start = phrase.getNoteStartTime(i) + pStart;
+                List<Integer> np = notePitches.get(start);
                 List<Note> cn = chords.get(start);
-                if(cn != null){
+                if(cn != null && !np.contains(note.getPitch())){
+                    np.add(note.getPitch());
+                    notePitches.put(start, np);
+                    
                     cn.add(note);
                     chords.put(start, cn);
                 }else{
+                    np = new ArrayList<Integer>();
                     cn = new ArrayList<Note>();
+                    np.add(note.getPitch());
+                    notePitches.put(start, np);
+                    
                     cn.add(note);
                     chords.put(start, cn);
+                    
                 }                   
             }
         }
         
+        List<Chord> chordArray = new ArrayList<Chord>();
         Set<Double> keys = chords.keySet();
-        for(Double d : keys){
+        List<Double> k = new ArrayList<Double>(keys);
+        Collections.sort(k);
+        System.out.println("Keys: " + keys.size());
+        for(Double d : k){
             List<Note> chord = chords.get(d);
-            System.out.println("Chord: ");
+            //chordArray.add(new Chord(chord,d));
+            System.out.print("Chord: ");
             for(Note n : chord){
-                System.out.print(n.getNote() + " ");
+                System.out.print(n.getNote() + "(pitch:"+ n.getPitch() +", Dur: " + n.getDuration() +") ");
+                
             }
-        }
+            System.out.println();
+        }    
         
+        List<List<Chord>> foundChordPatterns = findChordPatterns(chordArray, 5);
         
         return 0;
     }
 
+    
+    private List<List<Chord>> findChordPatterns(List<Chord> chords, int min){
+        
+        /**Best result and suffix lists*/
+        List<List<Chord>> bestResults = new ArrayList<List<Chord>>();
+        List<List<Chord>> suffixList = new ArrayList<List<Chord>>();
+        
+        int inputLength = chords.size();
+        
+        for(int i = 0; i < inputLength; i++){
+            List<Chord> nextSuffix = chords.subList(i, inputLength);
+            if(nextSuffix.size() >= min)
+                suffixList.add(nextSuffix);
+        }
+        
+        inputLength = suffixList.size();
+        
+        List<Chord> nextFound = new ArrayList<Chord>();
+        
+        int distance;
+        int checkFurter = 1;
+        
+        
+        return bestResults;       
+    }
+    
+    protected class Chord{
+        List<Integer> notes;
+        double duration;
+        
+        public Chord(List<Integer> notes, double duration){
+            this.notes = new ArrayList<Integer>();
+            this.notes = notes;
+            this.duration = duration;
+        }
+        
+        public Chord(){
+            this.notes = new ArrayList<Integer>();
+        }
+        
+        public void addNote(Integer pitch, double duration){
+            this.notes.add(pitch);
+            if(this.duration < duration)
+                this.duration = duration;
+        }
+        
+        @Override 
+        public boolean equals(Object o){
+            if(!(o instanceof Chord))
+                return false;
+            Chord c = (Chord)o;
+            if(!(c.notes.size() == notes.size()))
+                return false;
+            for(int i = 0; i < notes.size(); i++){
+                if(!(c.notes.get(i).equals(notes.get(i))))
+                    return false;
+            }
+            return true;
+        }
+    }
+    
 }
