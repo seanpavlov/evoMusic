8086508cb8df35f0212e9c28ba765a9ba6b73db6
Sean Pavlov <sean.pavlov@gmail.com>
2014-04-15 21:26:31 +0200

    Change Mutator-class so that iteration is handled inside sub mutators instead.

--- src/com/evoMusic/model/geneticAlgorithm/mutation/ISubMutator.java
+++ src/com/evoMusic/model/geneticAlgorithm/mutation/ISubMutator.java
@@ -15,7 +15,7 @@ public abstract class ISubMutator {
      * @param origPhrase is the phrase to be mutated
      * @return the mutated phrase
      */
-    abstract public void mutate(Song song, int noteIndex);
+    abstract public void mutate(Song song);
 
     /**
      * Get the probability of using the sub-mutator.
