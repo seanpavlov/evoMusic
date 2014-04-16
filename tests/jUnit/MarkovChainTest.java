4765bfc714e8642565322cc5f592feb326ec3a6e
Viktor Anderling <natanande@gmail.com>
2014-04-16 16:44:42 +0200

    Add test class for intervalTrack

--- tests/jUnit/MarkovChainTest.java
+++ tests/jUnit/MarkovChainTest.java
@@ -18,70 +18,72 @@ import com.evoMusic.util.TrackTag;
 
 public class MarkovChainTest {
     
-    private Song marioSong;
-    private Song marioTheme;
-    private List<Song> flutes;
-    private Song moonlight;
-    private MarkovChain markov;
-    private Song nyanCat;
-    private Song zelda;
-    private Song wilyStage;
-    private List<Song> multiSongTestList;
+//    private Song marioSong;
+//    private Song marioTheme;
+//    private List<Song> flutes;
+//    private Song moonlight;
+//    private MarkovChain markov;
+//    private Song nyanCat;
+//    private Song zelda;
+//    private Song wilyStage;
+//    private List<Song> multiSongTestList;
 
     @Before
     public void setUp() throws Exception {
-        flutes = new ArrayList<Song>();
-        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-1all.mid"));
-        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-2cou.mid"));
-        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-3sar.mid"));
-        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-4bou.mid"));
-        for(Song song : flutes) {
-            song.addTagToTrack(0, TrackTag.MELODY);
-        }
-        
-        multiSongTestList = new ArrayList<Song>();
-        
-        zelda = Translator.INSTANCE.loadMidiToSong("midifiles/zeldaALinkToThePast.mid");
-        zelda.addTagToTrack(0, TrackTag.BASELINE);
-        zelda.addTagToTrack(1, TrackTag.CHORDS);
-        zelda.addTagToTrack(3, TrackTag.MELODY);
-        
-        wilyStage = Translator.INSTANCE.loadMidiToSong("midifiles/mm2wily1.mid");
-        wilyStage.addTagToTrack(0, TrackTag.MELODY);
-        
-        marioSong = Translator.INSTANCE.loadMidiToSong("midifiles/super_mario_world_overworld.mid");
-        marioSong.addTagToTrack(0, TrackTag.MELODY);
-        marioTheme = Translator.INSTANCE.loadMidiToSong("midifiles/super_mario_bros_theme.mid");
-        marioTheme.addTagToTrack(0, TrackTag.MELODY);
-        moonlight = Translator.INSTANCE.loadMidiToSong("midifiles/mond_3.mid");
-        moonlight.addTagToTrack(0, TrackTag.MELODY);
-        nyanCat = Translator.INSTANCE.loadMidiToSong("midifiles/nyan_cat_cut.mid");
-        nyanCat.addTagToTrack(0, TrackTag.MELODY);
+//        flutes = new ArrayList<Song>();
+//        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-1all.mid"));
+//        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-2cou.mid"));
+//        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-3sar.mid"));
+//        flutes.add(Translator.INSTANCE.loadMidiToSong("midifiles/fp-4bou.mid"));
+//        for(Song song : flutes) {
+//            song.addTagToTrack(0, TrackTag.MELODY);
+//        }
+//        
+//        multiSongTestList = new ArrayList<Song>();
+//        
+//        zelda = Translator.INSTANCE.loadMidiToSong("midifiles/zeldaALinkToThePast.mid");
+//        zelda.addTagToTrack(0, TrackTag.BASELINE);
+//        zelda.addTagToTrack(1, TrackTag.CHORDS);
+//        zelda.addTagToTrack(3, TrackTag.MELODY);
+//        
+//        wilyStage = Translator.INSTANCE.loadMidiToSong("midifiles/mm2wily1.mid");
+//        wilyStage.addTagToTrack(0, TrackTag.MELODY);
+//        
+//        marioSong = Translator.INSTANCE.loadMidiToSong("midifiles/super_mario_world_overworld.mid");
+//        marioSong.addTagToTrack(0, TrackTag.MELODY);
+//        marioTheme = Translator.INSTANCE.loadMidiToSong("midifiles/super_mario_bros_theme.mid");
+//        marioTheme.addTagToTrack(0, TrackTag.MELODY);
+//        moonlight = Translator.INSTANCE.loadMidiToSong("midifiles/mond_3.mid");
+//        moonlight.addTagToTrack(0, TrackTag.MELODY);
+//        nyanCat = Translator.INSTANCE.loadMidiToSong("midifiles/nyan_cat_cut.mid");
+//        nyanCat.addTagToTrack(0, TrackTag.MELODY);
         
         
     }
 
     @Test
     public void test() {
-        //IntervalSong intervalSong = new IntervalSong(marioSong);
-        //Song newSong = intervalSong.toSong();
-        //Translator.INSTANCE.play(newSong);
         
-        //markov = new MarkovChain(flutes.get(2));
-        List<Song> nyanList = new ArrayList<Song>(1);
-        //nyanList.add(nyanCat);
-        //nyanList.add(flutes.get(2));
-        nyanList.add(zelda);
-        //nyanList.add(wilyStage);
-        //nyanList.add(moonlight);
-        //nyanList.add(marioTheme);
-        markov = new MarkovChain(2, nyanList);
-        Song newSong = markov.generateNew(100);
-        //Translator.INSTANCE.saveSongToMidi(newSong, "TestSaveNatan");
-        Translator.INSTANCE.play(newSong.getTrack(1));
-        //Translator.INSTANCE.play(zelda.getTrack(3).getPart());
-        //assertTrue(true);
-        //fail("Not yet implemented");
+        
+//        //IntervalSong intervalSong = new IntervalSong(marioSong);
+//        //Song newSong = intervalSong.toSong();
+//        //Translator.INSTANCE.play(newSong);
+//        
+//        //markov = new MarkovChain(flutes.get(2));
+//        List<Song> nyanList = new ArrayList<Song>(1);
+//        //nyanList.add(nyanCat);
+//        //nyanList.add(flutes.get(2));
+//        nyanList.add(zelda);
+//        //nyanList.add(wilyStage);
+//        //nyanList.add(moonlight);
+//        //nyanList.add(marioTheme);
+//        markov = new MarkovChain(2, nyanList);
+//        Song newSong = markov.generateNew(100);
+//        //Translator.INSTANCE.saveSongToMidi(newSong, "TestSaveNatan");
+//        Translator.INSTANCE.play(newSong.getTrack(1));
+//        //Translator.INSTANCE.play(zelda.getTrack(3).getPart());
+//        //assertTrue(true);
+//        //fail("Not yet implemented");
     }
 
 }
---
+++ tests/jUnit/blending/IntervalTrackTest.java
@@ -0,0 +1,39 @@
+package jUnit.blending;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IntervalTrackTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testIntervalTrackTrack() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIntervalTrackIntIntIntIntArrayDoubleArrayDoubleArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToTrack() {
+        fail("Not yet implemented");
+    }
+
+}
