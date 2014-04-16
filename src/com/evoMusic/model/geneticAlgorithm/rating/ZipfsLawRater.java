432e7dcb18e6f242ff89e94778e5f23718f8d174
Christian Svensson <crillesvensson22@gmail.com>
2014-04-16 14:54:14 +0200

    CPmedian/Cmedian

--- src/com/evoMusic/model/geneticAlgorithm/rating/ZipfsLawRater.java
+++ src/com/evoMusic/model/geneticAlgorithm/rating/ZipfsLawRater.java
@@ -47,11 +47,34 @@ public class ZipfsLawRater extends SubRater {
     }
     
     private void plot(List<Integer> values){
+        List<Integer> perfValues = new ArrayList<Integer>();
+        Integer perfTot = 0;
+        Integer tempTot = 0;
         List<Integer> temp = new ArrayList<Integer>();
+        List<Integer> perf = new ArrayList<Integer>();
+        
         for (int x = 0; x < values.size(); x++){
-            temp.add((x+1) * values.get(x));
+            perfValues.add(values.get(0) / (x+1));
         }
-        System.out.println(temp);
+        
+        for (int x = 0; x < values.size(); x++){
+            Integer v = (x+1) * values.get(x);
+            Integer vPerf = (x+1) * perfValues.get(x);
+            temp.add(v);
+            perf.add(vPerf);
+            tempTot += v;
+            perfTot += vPerf;
+        }
+        tempTot = tempTot / values.size();
+        perfTot = perfTot / perfValues.size();
+        System.out.println(perfValues);
+        System.out.println();
+        System.out.println(temp);       
+        System.out.println(perf);
+        System.out.println((double)Math.min(tempTot, perfTot) / (double)Math.max(tempTot, perfTot));
+        
+        System.out.println();
+        System.out.println();
         System.out.println();
     }
 
