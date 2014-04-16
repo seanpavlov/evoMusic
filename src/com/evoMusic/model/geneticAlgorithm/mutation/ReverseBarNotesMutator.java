433569ae0f12d07a32e3672c5719c5c05bdc9ff3
Sean Pavlov <sean.pavlov@gmail.com>
2014-04-16 16:22:50 +0200

    Fix Reverse Mutator and rename to ReverseBarNotesMutator.

---
+++ src/com/evoMusic/model/geneticAlgorithm/mutation/ReverseBarNotesMutator.java
@@ -0,0 +1,56 @@
+package com.evoMusic.model.geneticAlgorithm.mutation;
+
+
+
+import com.evoMusic.model.Song;
+import com.evoMusic.model.Track;
+
+public class ReverseBarNotesMutator extends ISubMutator {
+
+    /**
+     * Reverse a part of the track by selecting a number of notes and put them
+     * in the song in reversed order again.
+     * 
+     * @param mutationProbability
+     *            is the probability of the mutation.
+     * @param nbrOfAdditionalReversing
+     *            is the number of neighboring notes to current note that is
+     *            going to be part of of the reversing.
+     * @param nbrRange
+     *            is the range of neighboring notes it can be. It will range
+     *            between nbrOfAdditionalReversing and
+     *            nbrOfAdditionalReversing+nbrRange.
+     * @param withRhythmLength
+     *            is if it should take the rhythm length into the reversing.
+     *            True if it should and false otherwise.
+     */
+    public ReverseBarNotesMutator(double mutationProbability) {
+        super(mutationProbability);
+    }
+
+    /**
+     * Mutate the note with noteIndex of song.
+     */
+    @Override
+    public void mutate(Song individual) {
+        int nbrOfTracks = individual.getScore().getSize();
+        for (int track = 0; track < nbrOfTracks; track++) {
+            double nbrOfBars = individual.getScore().getPart(track).getEndTime() / 4.0;
+            Track currentTrack = new Track(individual.getScore().getPart(track));
+            for (int bar = 0; bar < nbrOfBars; bar++) {
+                if(Math.random() < getProbability()){
+                    Track currentBar = currentTrack.getSegment(bar*4, 4);
+                    int nbrOfPhrases = currentBar.getPart().size();
+                    for(int phrase = 0; phrase < nbrOfPhrases; phrase++){
+                        int nbrOfNotes = currentBar.getPart().getPhrase(phrase).size();
+                        for(int note = 0; note < nbrOfNotes/2; note++){
+                            int tempPitch = currentBar.getPart().getPhrase(phrase).getNote(note).getPitch();
+                            currentBar.getPart().getPhrase(phrase).getNote(note).setPitch(currentBar.getPart().getPhrase(phrase).getNote(nbrOfNotes-note-1).getPitch());
+                            currentBar.getPart().getPhrase(phrase).getNote(nbrOfNotes-note-1).setPitch(tempPitch);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
