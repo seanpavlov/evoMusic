ecbfefa0ed1d0e12293c90a5af72b759bad19284
Sean Pavlov <sean.pavlov@gmail.com>
2014-04-17 14:17:50 +0200

    Adapt selection to list instead of array.

--- src/com/evoMusic/model/geneticAlgorithm/selection/RouletteWheelSelection.java
+++ src/com/evoMusic/model/geneticAlgorithm/selection/RouletteWheelSelection.java
@@ -1,5 +1,9 @@
 package com.evoMusic.model.geneticAlgorithm.selection;
 
+import java.util.List;
+
+import com.evoMusic.model.geneticAlgorithm.Individual;
+
 public class RouletteWheelSelection implements ISelection {
     private int nbrOfIndividuals;
     private double[] probabilityList;
@@ -11,15 +15,15 @@ public class RouletteWheelSelection implements ISelection {
      * 
      * @param populationRating is the list of ratings
      */
-    public RouletteWheelSelection(double[] populationRating) {
+    public RouletteWheelSelection(List<Individual> populationRating) {
         double sumOfAllFitness = 0;
-        nbrOfIndividuals = populationRating.length;
+        nbrOfIndividuals = populationRating.size();
         for (int individual = 0; individual < nbrOfIndividuals; individual++) {
-            sumOfAllFitness += populationRating[individual];
+            sumOfAllFitness += populationRating.get(individual).getRating();
         }
         probabilityList = new double[nbrOfIndividuals];
         for (int i = 0; i < nbrOfIndividuals; i++) {
-            probabilityList[i] = populationRating[i] / sumOfAllFitness;
+            probabilityList[i] = populationRating.get(i).getRating() / sumOfAllFitness;
         }
     }
 
