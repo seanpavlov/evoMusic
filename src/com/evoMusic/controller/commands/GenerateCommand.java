0a2114050fd4c20599f5b73530c3e51639e4ae1a
Sean Pavlov <sean.pavlov@gmail.com>
2014-04-18 21:26:23 +0200

    Update genetic algorithm class so that it works like the real GA.

--- src/com/evoMusic/controller/commands/GenerateCommand.java
+++ src/com/evoMusic/controller/commands/GenerateCommand.java
@@ -40,67 +40,88 @@ public class GenerateCommand extends AbstractCommand {
     private List<Song> selectedSongs;
     private Semaphore finished = new Semaphore(0);
     private Parameters c = Parameters.getInstance();
-    
+
     /**
      * Creates the generate command
+     * 
      * @param selectedSongs
-     *              reference to the songs selected to be used for generating
-     *              individuals
+     *            reference to the songs selected to be used for generating
+     *            individuals
      */
     public GenerateCommand(List<Song> selectedSongs) {
         this.selectedSongs = selectedSongs;
     }
-    
+
     @Override
     public boolean execute(String[] args) {
-        if(selectedSongs.size() < 2) {
+        if (selectedSongs.size() < 2) {
             return false;
         }
         final int iterations = Integer.parseInt(args[0]);
         List<ISubMutator> allMut = new ArrayList<ISubMutator>();
-        allMut.add(new RandomNoteMutator(c.MUTATOR_RANDOM_NOTE_PROBABILITY, c.MUTATOR_RANDOM_NOTE_STEP_RANGE));
-        allMut.add(new OctaveMutator(c.MUTATOR_OCTAVE_PROBABILITY, c.MUTATOR_OCTAVE_RANGE));
-        //allMut.add(new ReverseMutator(c.MUTATOR_REVERSE_PROBABILITY, c.MUTATOR_REVERSE_NBR_OF_NEIGHBORS, c.MUTATOR_REVERSE_RANGE, true));
-        allMut.add(new ScaleOfFifthMutator(c.MUTATOR_SCALE_OF_FIFTH_PROBABILITY, c.MUTATOR_SCALE_OF_FIFTH_RANGE));
-        //allMut.add(new SimplifyMutator(c.MUTATOR_SIMPLIFY_PROBABILITY, c.MUTATOR_SIMPLIFY_NBR_OF_NEIGHBORS, c.MUTATOR_SIMPLIFY_PROBABILITY));
+        allMut.add(new RandomNoteMutator(c.MUTATOR_RANDOM_NOTE_PROBABILITY,
+                c.MUTATOR_RANDOM_NOTE_STEP_RANGE));
+        allMut.add(new OctaveMutator(c.MUTATOR_OCTAVE_PROBABILITY,
+                c.MUTATOR_OCTAVE_RANGE));
+        // allMut.add(new ReverseMutator(c.MUTATOR_REVERSE_PROBABILITY,
+        // c.MUTATOR_REVERSE_NBR_OF_NEIGHBORS, c.MUTATOR_REVERSE_RANGE, true));
+        allMut.add(new ScaleOfFifthMutator(
+                c.MUTATOR_SCALE_OF_FIFTH_PROBABILITY,
+                c.MUTATOR_SCALE_OF_FIFTH_RANGE));
+        // allMut.add(new SimplifyMutator(c.MUTATOR_SIMPLIFY_PROBABILITY,
+        // c.MUTATOR_SIMPLIFY_NBR_OF_NEIGHBORS,
+        // c.MUTATOR_SIMPLIFY_PROBABILITY));
         List<SubRater> subRaters = new LinkedList<SubRater>();
-        
-        subRaters.add(new MelodyRepetionRater(c.RATER_MELODY_REPETITION_WEIGHT));
+
+        subRaters
+                .add(new MelodyRepetionRater(c.RATER_MELODY_REPETITION_WEIGHT));
         subRaters.add(new ScaleWhizz(c.RATER_SCALE_WEIGHT));
         subRaters.add(new BeatRepetitionRater(c.RATER_BEAT_REPETITION_WEIGHT));
-        subRaters.add(new ChordRepetitionRater(c.RATER_CHORD_REPETITION_WEIGHT));
+        subRaters
+                .add(new ChordRepetitionRater(c.RATER_CHORD_REPETITION_WEIGHT));
         DrCross crossover = new DrCross(1);
         subRaters.add(new CrazyNoteOctaveRater(c.RATER_CRAZY_OCTAVE_WEIGHT));
-        subRaters.add(new MelodyDirectionStabilityRater(c.RATER_MELODY_DIRECTION_WEIGHT));
+        subRaters.add(new MelodyDirectionStabilityRater(
+                c.RATER_MELODY_DIRECTION_WEIGHT));
         subRaters.add(new PitchVarietyRater(c.RATER_PITCH_VARIETY_WEIGHT));
         subRaters.add(new MelodyDirectionRater(c.RATER_PITCH_DIRECTION_WEIGHT));
-        subRaters.add(new MelodyNoteDensityRater(c.RATER_MELODY_NOTE_DENSITY_WEIGHT));
-        subRaters.add(new RhythmicVarietyRater(c.RATER_RHYTHMIC_VARIETY_WEIGHT));
+        subRaters.add(new MelodyNoteDensityRater(
+                c.RATER_MELODY_NOTE_DENSITY_WEIGHT));
+        subRaters
+                .add(new RhythmicVarietyRater(c.RATER_RHYTHMIC_VARIETY_WEIGHT));
         subRaters.add(new NoSilenceRater(c.RATER_NO_SILENCE_WEIGHT));
-        subRaters.add(new MelodyPitchRangeRater(c.RATER_MELODY_PITCH_RANGE_WEIGHT));
-        subRaters.add(new RepeatedPitchDensityRater(c.RATER_REPEATED_PITCH_DENSITY_WEIGTH));
-        subRaters.add(new MelodyRestDensityRater(c.RATER_MELODY_REST_DENSITY_WEIGHT));
+        subRaters.add(new MelodyPitchRangeRater(
+                c.RATER_MELODY_PITCH_RANGE_WEIGHT));
+        subRaters.add(new RepeatedPitchDensityRater(
+                c.RATER_REPEATED_PITCH_DENSITY_WEIGTH));
+        subRaters.add(new MelodyRestDensityRater(
+                c.RATER_MELODY_REST_DENSITY_WEIGHT));
         subRaters.add(new ZipfsLawRater(c.RATER_ZIPFS_LAW_WEIGHT));
-        
-        final GeneticAlgorithm ga = new GeneticAlgorithm(selectedSongs, new Mutator(allMut, c.MUTATION_INITIAL_PROBABILITY, c.MUTATION_MINIMUM_PROBABILITY, c.MUTATION_PROBABILITY_RATIO), crossover, new Rater(subRaters));
-        
-        ga.setMinimumIterations(iterations);
+
+        final GeneticAlgorithm ga = new GeneticAlgorithm(selectedSongs,
+                new Mutator(allMut, c.MUTATION_INITIAL_PROBABILITY,
+                        c.MUTATION_MINIMUM_PROBABILITY,
+                        c.MUTATION_PROBABILITY_RATIO), crossover, new Rater(
+                        subRaters), 100, 2, 4);
+
+//        ga.setMinimumIterations(iterations);
         System.out.println("Start iterating");
-        
+
         runProgress(ga, iterations);
-        ga.iterate();
+        ga.generateGenerations(iterations);
         finished.acquireUninterruptibly();
-        
-        //Translator.INSTANCE.saveSongToMidi(ga.getBest(), "best");
-        Translator.INSTANCE.play(ga.getBest());
+
+        Translator.INSTANCE.saveSongToMidi(ga.getBestSong(), "Bob-Slide64");
+        Translator.INSTANCE.play(ga.getBestSong());
         return true;
     }
-    
+
     private void runProgress(final GeneticAlgorithm ga, final int iterations) {
         new Thread(new Runnable() {
             final int width = 50;
             final int height = 20;
             final boolean eclipseHack = true;
+
             @Override
             public void run() {
                 try {
@@ -113,22 +134,24 @@ public class GenerateCommand extends AbstractCommand {
                 String newLine = "\r";
                 if (eclipseHack) {
                     StringBuilder newLines = new StringBuilder();
-                    for (; i < height; i ++){
+                    for (; i < height; i++) {
                         newLines.append("\n");
                     }
                     newLine = newLines.toString();
-                } 
-                
+                }
+
                 System.out.print(newLine + "[");
                 i = 0;
-                for (; i < (int)((1.0*ga.getIterationsDone() / iterations )*width); i++) {
-                  System.out.print("#");
+                for (; i < (int) ((1.0 * ga.getCurrentIteration() / iterations) * width); i++) {
+                    System.out.print("#");
                 }
                 for (; i < width; i++) {
-                  System.out.print(" ");
+                    System.out.print(" ");
                 }
-                System.out.print("] "+ga.getIterationsDone()*100 / iterations + "%" + " | " + "Best rating: " + ga.getBestRating());
-                if (ga.getIterationsDone() / iterations == 1) {
+                System.out.print("] " + ga.getCurrentIteration() * 100
+                        / iterations + "%" + " | " + "Best rating: "
+                        + ga.getBestRating());
+                if (ga.getCurrentIteration() / iterations == 1) {
                     System.out.println();
                     finished.release();
                     return;
@@ -138,7 +161,7 @@ public class GenerateCommand extends AbstractCommand {
             }
         }).start();
     }
-    
+
     @Override
     public Set<String> getArgSet() {
         return Sets.newHashSet("numberOfIterations");
--- src/com/evoMusic/model/geneticAlgorithm/GeneticAlgorithm.java
+++ src/com/evoMusic/model/geneticAlgorithm/GeneticAlgorithm.java
@@ -9,29 +9,53 @@ import com.evoMusic.model.geneticAlgorithm.rating.Rater;
 import com.evoMusic.model.geneticAlgorithm.selection.RouletteWheelSelection;
 
 public class GeneticAlgorithm {
-    private List<Song> originalSongs;
     private List<Individual> bestIndividuals;
-    private Mutator mutator;
+
     private DrCross crossover;
+    private Mutator mutator;
     private Rater rater;
     private RouletteWheelSelection rwSelect;
-    int populationSize, nbrOfGenerations, nbrOfBestIndividuals,
-            nbrOfCrossoverIndividuals;
+    int populationSize, nbrOfGenerations, nbrOfElitismSongs,
+            nbrOfCrossoverIndividuals, currentIteration;
     double ratingThreshold;
+    private List<Song> inputSongs;
 
+    /**
+     * Initiate the genetic algorithm with the necessary parameters.
+     * 
+     * @param inputSongs
+     *            is the songs that the user choose.
+     * @param mutator
+     *            is the mutator containing all sub mutators.
+     * @param crossover
+     *            is the crossover used.
+     * @param rater
+     *            is the rater containing all sub raters.
+     * @param populationSize
+     *            is the size of the population in each generation
+     * @param nbrOfElitismSongs
+     *            is the number of songs going to next generation through
+     *            elitism.
+     * @param nbrOfCrossoverIndividuals
+     *            is the number of individuals used in a crossover.
+     */
     public GeneticAlgorithm(List<Song> inputSongs, Mutator mutator,
             DrCross crossover, Rater rater, int populationSize,
-            int nbrOfBestIndividuals, int nbrOfCrossoverIndividuals,
-            int nbrOfGenerations) {
-        originalSongs = inputSongs;
-        this.mutator = mutator;
+            int nbrOfElitismSongs, int nbrOfCrossoverIndividuals) {
+        this.inputSongs = inputSongs;
         this.crossover = crossover;
+        this.mutator = mutator;
         this.rater = rater;
         this.populationSize = populationSize;
-        this.nbrOfGenerations = nbrOfGenerations;
-        this.nbrOfBestIndividuals = nbrOfBestIndividuals;
+        this.nbrOfElitismSongs = nbrOfElitismSongs;
         this.nbrOfCrossoverIndividuals = nbrOfCrossoverIndividuals;
+        this.inputSongs = inputSongs;
+    }
+
+    public void generateGenerations(int nbrOfGenerations) {
+        this.nbrOfGenerations = nbrOfGenerations;
         bestIndividuals = new ArrayList<Individual>();
+        currentIteration = 0;
 
         // TODO: Initialize weight based on input songs. Decide how we are going
         // to do this.
@@ -41,36 +65,41 @@ public class GeneticAlgorithm {
 
         List<Individual> ratedFirstGeneration = ratePopulation(firstGeneration);
 
-        selectBestIndividuals(ratedFirstGeneration);
+        selectElitismSongs(ratedFirstGeneration);
 
         List<Song> tempPopulation;
         List<Individual> population;
-        
+
         for (int generation = 0; generation < nbrOfGenerations; generation++) {
+            currentIteration++;
             tempPopulation = new ArrayList<Song>();
+            for(int i = 0; i < bestIndividuals.size(); i++){
+                tempPopulation.add(bestIndividuals.get(i).getSong().copy());
+            }
             population = new ArrayList<Individual>();
 
             // Generate individuals - crossover on best population
             // Rate them and put in best population if better
             List<Song> tempList = new ArrayList<Song>();
             crossover.setParents(individualToSong(bestIndividuals));
-            for(int i = 0; i < populationSize; i++){
-                if(tempList.size() == 0){
+            for (int i = 0; i < populationSize-bestIndividuals.size(); i++) {
+                if (tempList.size() == 0) {
                     tempList = crossover.crossIndividuals();
                 }
                 tempPopulation.add(tempList.remove(0));
             }
             population = ratePopulation(tempPopulation);
-            selectBestIndividuals(population);
+            selectElitismSongs(population);
 
             // Select individuals to be crossovered
             tempPopulation = new ArrayList<Song>();
             tempList = new ArrayList<Song>();
             rwSelect = new RouletteWheelSelection(population);
-            for(int i = 0; i < populationSize; i++){
-                if(tempList.size() == 0){
-                    for(int j = 0; j < nbrOfCrossoverIndividuals; j++){
-                        tempList.add(population.get(rwSelect.select()).getSong());
+            for (int i = 0; i < populationSize; i++) {
+                if (tempList.size() == 0) {
+                    for (int j = 0; j < nbrOfCrossoverIndividuals; j++) {
+                        tempList.add(population.get(rwSelect.select())
+                                .getSong());
                     }
                     crossover.setParents(tempList);
                     tempList = crossover.crossIndividuals();
@@ -79,18 +108,43 @@ public class GeneticAlgorithm {
                 mutator.mutate(tempPopulation.get(i));
             }
             population = ratePopulation(tempPopulation);
-            selectBestIndividuals(population);
+            selectElitismSongs(population);
         }
+    }
+
+    public Song getBestSong() {
+        return bestIndividuals.get(0).getSong();
+    }
+
+    public double getBestRating() {
+        if (bestIndividuals.size() == 0) {
+            return 0;
+        } else {
+            return bestIndividuals.get(0).getRating();
+        }
+    }
+
+    public int getCurrentIteration() {
+        return currentIteration;
     }
 
     private List<Song> generateFirstGeneration() {
         // TODO: Insert markov initialization here
 
-        for (int individual = 0; individual < populationSize; individual++) {
-            // TODO: Insert generate individual and put in list here
-            // population[individual] = markov
+        List<Song> population = new ArrayList<Song>();
+        // for (int individual = 0; individual < populationSize; individual++) {
+        // TODO: Insert generate individual and put in list here
+        // population[individual] = markov
+        // }
+        crossover.setParents(inputSongs);
+        List<Song> tempList = new ArrayList<Song>();
+        for (int i = 0; i < populationSize; i++) {
+            if (tempList.size() == 0) {
+                tempList = crossover.crossIndividuals();
+            }
+            population.add(tempList.remove(0));
         }
-        return null;
+        return population;
     }
 
     private List<Individual> ratePopulation(List<Song> population) {
@@ -102,26 +156,29 @@ public class GeneticAlgorithm {
         return ratedPopulation;
     }
 
-    private void selectBestIndividuals(List<Individual> population) {
-        for (int individual = 0; individual < populationSize; individual++) {
-            check_if_better_loop: for (int bestIndividual = 0; bestIndividual < nbrOfBestIndividuals; bestIndividual++) {
+    private void selectElitismSongs(List<Individual> population) {
+        int nbrOfIndividuals = population.size();
+        for (int individual = 0; individual < nbrOfIndividuals; individual++) {
+            for (int i = 0; i < bestIndividuals.size(); i++) {
                 if (population.get(individual).getRating() > bestIndividuals
-                        .get(bestIndividual).getRating()) {
-                    for (int shift = nbrOfBestIndividuals; shift > bestIndividual; shift--) {
-                        bestIndividuals.set(shift,
-                                bestIndividuals.get(shift - 1));
+                        .get(i).getRating()) {
+                    if (bestIndividuals.size() < nbrOfElitismSongs) {
+                        bestIndividuals.add(i, population.get(individual));
+                    } else {
+                        bestIndividuals.add(i, population.get(individual));
+                        bestIndividuals.remove((bestIndividuals.size() - 1));
                     }
-                    bestIndividuals.set(bestIndividual,
-                            population.get(individual));
-                    break check_if_better_loop;
                 }
+            }
+            if (bestIndividuals.size() == 0) {
+                bestIndividuals.add(population.get(individual));
             }
         }
     }
-    
-    private List<Song> individualToSong(List<Individual> individuals){
-        List<Song> tempList= new ArrayList<Song>();
-        for(int i = 0; i < individuals.size(); i++){
+
+    private List<Song> individualToSong(List<Individual> individuals) {
+        List<Song> tempList = new ArrayList<Song>();
+        for (int i = 0; i < individuals.size(); i++) {
             tempList.add(individuals.get(i).getSong());
         }
         return tempList;
