f5e1d4ec4ed6d31c9eb38f214ec1b04b0d449c83
Viktor Anderling <natanande@gmail.com>
2014-05-02 16:04:20 +0200

    Improve RandomInitiator

--- src/com/evoMusic/model/geneticAlgorithm/RandomInitiator.java
+++ src/com/evoMusic/model/geneticAlgorithm/RandomInitiator.java
@@ -15,53 +15,101 @@ import jm.music.data.Phrase;
 import jm.music.data.Score;
 
 public class RandomInitiator {
+
+    // 4th, 8th, 16th note etc.
+    private static final int SMALLEST_NOTE_TYPE = 32;
+    private static final double REST_PROBABILITY = 0.2;
+    private static final int NOTE_DYNAMICS = 64;
+
     private Song song;
-    double maxLength;
-    Random random = new Random();
-    
-    public RandomInitiator(double maxLength){
+    private double maxLength;
+    private Random random = new Random();
+
+    public RandomInitiator(double maxLength) {
         this.song = new Song(new Score());
-        song.getScore().setTempo(120);
+        this.song.getScore().setTempo(120);
         this.maxLength = maxLength;
     }
-    
+
     /**
-     * Generates a MelodyTrack and returns the song in which 
-     * this track is added to.
+     * Generates a MelodyTrack and returns the song in which this track is added
+     * to.
      * 
      * @return
      */
-    public Song generateMelody(){
+    public Song generateMelody() {
         Part melodyPart = new Part();
         Track melodyTrack = new Track(melodyPart);
-        int nrOfPhrases = (int) Math.random() * 6 + 1; 
-        
-        //randomize number of phrases
-        for (int i = 0; i < nrOfPhrases; i++){
+        int nrOfPhrases = (int) Math.random() * 6 + 1;
+
+        // randomize number of phrases
+        for (int i = 0; i < nrOfPhrases; i++) {
             Phrase phrase = new Phrase();
             phrase.setStartTime(0);
-            
-            //add notes while length is less than maxlength
+
+            // add notes while length is less than maxlength
             do {
                 Note n = new Note();
-                double noteLength = (random.nextInt(4) + 1) / 2;
+                // double noteLength = (random.nextInt(8) + 1) / 4;
+                double noteLength = getRandomNoteLength(true);
                 n.setDuration(noteLength);
                 n.setRhythmValue(noteLength);
 
-                if (random.nextInt(10) + 1 % 10 == 0){
+                if (random.nextDouble() < REST_PROBABILITY) {
                     n.setPitch(Note.REST);
                 } else {
-                    n.setPitch(random.nextInt(80) + 22);
+                    n.setPitch(getRandomPitch(true));
+//                    n.setPitch(random.nextInt(80) + 22);
                 }
-                n.setDynamic(64);
+                n.setDynamic(NOTE_DYNAMICS);
                 phrase.add(n);
             } while (phrase.getEndTime() < maxLength);
-            
+
             melodyPart.add(phrase);
         }
 
         melodyTrack.setTag(TrackTag.MELODY);
         song.addTrack(melodyTrack);
         return song;
+    }
+
+    private double getRandomNoteLength(boolean curveControlled) {
+        int noteLength;
+        
+        if (curveControlled) {
+            double x = random.nextDouble() * 2;
+    
+            /*
+             * A 3rd grade function distributing values between 1 and
+             * smallestNoteType, with values closer to the middle having higher
+             * probability.
+             */
+            noteLength = (int) ((Math.pow((x - 1), 3) + 1) * 0.5
+                    * SMALLEST_NOTE_TYPE + 1);
+        } else {
+            noteLength = (int)((random.nextDouble() + 1) * SMALLEST_NOTE_TYPE);
+        }
+
+        return 4.0 / noteLength;
+    }
+
+    private int getRandomPitch(boolean curveControlled) {
+        int notePitch;
+        
+        if (curveControlled) {
+          double x = random.nextDouble() * 2;
+
+          /*
+           * A 3rd grade function distributing values between 1 and
+           * including 127, with values closer to the middle having higher
+           * probability.
+           */
+          notePitch = (int) ((Math.pow((x - 1), 3) + 1) * 0.5 * 128);
+            
+        } else {
+            notePitch = (int)(random.nextDouble() * 128);
+        }
+
+        return notePitch;
     }
 }
